adf7d9c255618c1b4897f7419d5d3af871e3ba14
payette28 <133899413+payette28@users.noreply.github.com>
2023-09-06 19:58:19 -0400

    Update movie-view.jsx
    if applied adds proptypes and connections to api

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,10 @@
-export const MovieView = ({movie, onBackClick}) => {
+import PropTypes from 'prop-types';
+
+export const MovieView = ({ movie, onBackClick }) => {
 	return (
 		<div>
 			<div>
-				<img src={movie.image} height="300px"/>
+				<img src={movie.imagepath} height="300px" />
 			</div>
 			<div>
 				<span>Title: </span>
@@ -14,13 +16,33 @@ export const MovieView = ({movie, onBackClick}) => {
 			</div>
 			<div>
 				<span>Genre: </span>
-				<span>{movie.genre}</span>
+				<span>{movie.genre.name}</span>
 			</div>
 			<div>
 				<span>Director: </span>
 				<span>{movie.director}</span>
 			</div>
+			<div>
+				<span>Featured: </span>
+				<span>{movie.featured}</span>
+			</div>
 			<button onClick={onBackClick}>Back</button>
 		</div>
 	);
 }; 
+
+MovieView.propTypes = {
+	movie: PropTypes.shape({
+	  imagepath: PropTypes.string.isRequired,
+	  title: PropTypes.string.isRequired,
+	  genre: PropTypes.shape({
+		name: PropTypes.string.isRequired
+	  }),
+	  description: PropTypes.string.isRequired,
+	  director: PropTypes.shape({
+		name: PropTypes.string.isRequired
+	  }),
+	  featured: PropTypes.string.isRequired
+	}).isRequired,
+	onBackClick: PropTypes.func.isRequired
+};
