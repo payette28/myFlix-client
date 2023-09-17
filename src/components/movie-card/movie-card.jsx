524ae53c10a806b909a245bd78af3ebec4a998b9
payette28 <133899413+payette28@users.noreply.github.com>
2023-09-17 18:06:40 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,32 @@
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
 
-export const MovieCard = ({movie, onMovieClick}) => {
+export const MovieCard = ({ movie, onMovieClick }) => {
 	return (
-		<div
-			onClick={() => {
-				onMovieClick(movie);
-			}}
-		>
-			{movie.title}
-		</div>
+		<Card className='h-100'>
+			<Card.Img variant='top' src={movie.imagePath} />
+			<Card.Body>
+				<Card.Title>
+					{movie.title}
+				</Card.Title>
+				<Card.Text>
+					{movie.director}
+					{movie.genre}
+				</Card.Text>
+				<Button
+					onClick={() => onMovieClick(movie)}
+					variant='link'
+				>
+					Open
+				</Button>
+			</Card.Body>
+		</Card>
 	);
 };
 
 MovieCard.propTypes = {
 	movie: PropTypes.shape({
-	  title: PropTypes.string.isRequired
+		title: PropTypes.string.isRequired
 	}).isRequired,
 	onMovieClick: PropTypes.func.isRequired
-  };
+};
