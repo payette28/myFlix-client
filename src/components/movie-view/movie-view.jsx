904b3c0166b24068298ce2851b784cb13d737240
payette28 <133899413+payette28@users.noreply.github.com>
2023-09-21 23:37:16 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,13 @@
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import './movie-view.scss';
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movies }) => {
+    const { movieId } = useParams();
+
+    const movie = movies.find((m) => m.id === movieId);
+
 	return (
 		<div>
 			<div>
@@ -27,13 +33,14 @@ export const MovieView = ({ movie, onBackClick }) => {
 				<span>Featured: </span>
 				<span>{movie.featured}</span>
 			</div>
-			<button
-                onClick={onBackClick}
-                className='back-button'
-                style={{ cursor: 'pointer' }}
-            >
-                Back
-            </button>
+			            <Link to={'/'}>
+                <button
+                    className='back-button'
+                    style={{ cursor: 'pointer' }}
+                >
+                    Back
+                </button>
+            </Link>
 		</div>
 	);
 };
